13701c35be6e2c884dc7f95b3209e56265198f35
Elna JP <eriosdd@gmail.com>
2022-08-07 10:34:32 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    return renderBooks(json)
+  });
+  return fetch;
   
 }
 
